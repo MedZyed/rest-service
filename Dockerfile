577d87bbf8757ccf3742d9d1b55a545742a208f6
MohamedZyedMEKKI <MohamedZyedMEKKI@DESKTOP-TBCR5G2>
2022-09-05 14:55:31 +0200

    prepare backend image

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,43 @@
+#FROM openjdk:8-jdk-alpine
+#
+#ENV DB_SERVER serene_williamson
+#
+#MAINTAINER Fakher SOUAYEH
+#
+#COPY build/libs/rest-service-1.1.0-SNAPSHOT.jar demo-backend-1.1.0.jar
+#
+#ENTRYPOINT ["java","-jar","/demo-backend-1.1.0.jar"]
+
+
+
+
+# using multistage docker build
+# ref: https://docs.docker.com/develop/develop-images/multistage-build/
+    
+# temp container to build using gradle
+FROM gradle:7.5.1-jdk11-alpine AS TEMP_BUILD_IMAGE
+ENV APP_HOME /usr/app/
+#ENV DB_SERVER="172.17.0.2"
+WORKDIR $APP_HOME
+COPY build.gradle settings.gradle $APP_HOME
+  
+COPY gradle $APP_HOME/gradle
+COPY --chown=gradle:gradle . /home/gradle/src
+USER root
+RUN chown -R gradle /home/gradle/src
+    
+RUN gradle build || return 0
+COPY . .
+RUN gradle clean build
+    
+# actual container
 FROM openjdk:8-jdk-alpine
-MAINTAINER Fakher SOUAYEH
-COPY build/libs/rest-service-1.1.0-SNAPSHOT.jar demo-backend-1.1.0.jar
-ENTRYPOINT ["java","-jar","/demo-backend-1.1.0.jar"]
+ENV ARTIFACT_NAME rest-service-1.1.0-SNAPSHOT.jar
+ENV APP_HOME /usr/app/
+#ENV DB_SERVER="172.17.0.2"
+    
+WORKDIR $APP_HOME
+COPY --from=TEMP_BUILD_IMAGE $APP_HOME/build/libs/$ARTIFACT_NAME .
+    
+EXPOSE 8080
+ENTRYPOINT exec java -jar ${ARTIFACT_NAME}
--- src/main/java/com/example/restservice/heroes/model/Hero.java
+++ src/main/java/com/example/restservice/heroes/model/Hero.java
@@ -4,48 +4,51 @@ import java.util.Objects;
 
 public class Hero {
 
-    private long id;
-    private String name;
+	private long id;
+	private String name;
 
-    public Hero(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+	public Hero(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
 
-    public long getId() {
-        return id;
-    }
+	public Hero() {
+		super();
+	}
 
-    public void setId(long id) {
-        this.id = id;
-    }
+	public long getId() {
+		return id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public void setId(long id) {
+		this.id = id;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Hero hero = (Hero) o;
-        return id == hero.id && name.equals(hero.name);
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name);
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Hero hero = (Hero) o;
+		return id == hero.id && name.equals(hero.name);
+	}
 
-    @Override
-    public String toString() {
-        return "Hero{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+
+	@Override
+	public String toString() {
+		return "Hero{" + "id=" + id + ", name='" + name + '\'' + '}';
+	}
 }
